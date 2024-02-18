30587e45b68d3b2f1e6bccce20d817562e8a06fe
Giordano <giordanobrunotj@hotmail.com>
2024-02-18 10:48:09 +0000

    css

--- project/main.py
+++ project/main.py
@@ -1,4 +1,3 @@
-
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from werkzeug.security import gen_salt
@@ -7,19 +6,11 @@ import random, string
 from .models import User, Items
 
 main = Blueprint('main', __name__)
-# def generateID():
-#     alpha = string.ascii_letters
-#     for i in range(4):
 
 @main.route('/')
 def index():
     
     return render_template('index.html')
-
-# @main.route("/home")
-# @login_required
-# def home():
-#     return render_template('index.html')
 
 @main.route("/home", methods=['POST'])
 @login_required
@@ -28,18 +19,20 @@ def home():
     if current_user.is_authenticated:
         item = request.form.get('Item') 
         price = request.form.get('PriceItem')
-        name  = current_user.name
+        email  = current_user.email
          
-        new_item = Items(name=name, item=item, price =price)
+        new_item = Items(email=email, item=item, price =price)
         db.session.add(new_item)
         db.session.commit()
  
-    return render_template('index.html', test = "hi")
+    return render_template('index.html')
 
 @main.route('/profile')
 @login_required
 def profile():
+    item = Items.query.filter_by(email = current_user.email).all()
+    total =0
+    for i in item:
+        total = total+i.price
+    return render_template('profile.html',name=current_user.name, item = item, total = total)
 
-    return render_template('profile.html',name=current_user.name)
-
-# @main.route("/")
--- project/models.py
+++ project/models.py
@@ -9,7 +9,7 @@ class User(UserMixin,db.Model):
 
 class Items(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000))
+    email = db.Column(db.String(1000))
     item = db.Column(db.String(100))
     price = db.Column(db.Integer)
 
