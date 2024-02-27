699e51598aa500e5b1a6704224f7d406642781c0
Giordano <giordanobrunotj@hotmail.com>
2024-02-27 18:05:57 +0000

    windows not working1

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__) 
+    app = Flask(__name__)  
 
     app.config['SECRET_KEY'] = 'giordano-secrets'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
