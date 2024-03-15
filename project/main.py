1c329274a7a35a722e35f7f3f0f3ed0578f042c3
Giordano <giordanobrunotj@hotmail.com>
2024-03-15 16:07:29 +0000

    currentTripIdCpy not working

--- project/main.py
+++ project/main.py
@@ -7,21 +7,24 @@ from .models import User, Items, Trip
 
 main = Blueprint('main', __name__)
 
+currentTripId = None
+
 @main.route('/') 
 def index():
-    
+    # currentTripId = None
     return render_template('index.html')
 
 @main.route('/trips') 
 @login_required 
 def trips():
+    # currentTripId = None
     trip = Trip.query.filter_by(email = current_user.email).all()
     return render_template('trips.html', trip =trip)
 
 @main.route("/trips", methods=['POST'])
 @login_required
 def trips_post(): 
-
+    # currentTripId = None
     if current_user.is_authenticated:
         # add trip details
         TripName = request.form.get('TripName') 
@@ -44,32 +47,50 @@ def trips_post():
             return render_template('profile.html',name=current_user.name, item = item, total = total)
 
 
-@main.route('/myexpenses/<tripId>', methods = ["get"])
-@login_required
-def myexpenses(tripId):
-    item = Items.query.filter_by(email = current_user.email, tripId = tripId).all()
-    total =0
+@main.route('/myexpenses')
+@login_required 
+def myexpenses():
+
+
+    if currentTripId == None:
+        return redirect(url_for("main.trips_post"))
+    
+    currentTripIdCopy = currentTripId
+    currentTripId = None
+    
+    item = Items.query.filter_by(email = current_user.email, tripId = currentTripIdCopy).all()
+    total =0 
+
     for i in item:
-        total = total+i.price
-    return render_template('myexpenses.html',name=current_user.name, item = item, total = total)
+        try:
+            total = total+i.price
+        except:
+            continue
+
+    return render_template('myexpenses.html',name=current_user.name, item = item, total = total, tripId = currentTripIdCopy, tripName = Trip.startdate)
 
 
 @main.route('/myexpenses/<tripId>', methods = ["POST", "get"])
 @login_required
 def myexpenses_post(tripId):
 
-    if current_user.is_authenticated:
+    global currentTripId 
+    currentTripId = tripId
+
+    if current_user.is_authenticated: 
         item = request.form.get('Item') 
         price = request.form.get('PriceItem')
         date = request.form.get('DateItem')
-        email  = current_user.email
-       
-        new_item = Items(email=email, item=item, price =price, date =date)
-        db.session.add(new_item)
-        db.session.commit()
-        flash('Expense added')
+        
+        if item != None:
+            new_item = Items(email=current_user.email, item=item, price =price, date =date, tripId = tripId)
+            db.session.add(new_item)
+            db.session.commit()
+            flash('Expense added')
 
-    return redirect(url_for("main.myexpenses", tripId = tripId))
+    return redirect(url_for("main.myexpenses")) 
+
+
 
 @main.route('/delete', methods = ["POST"])
 def delete():
