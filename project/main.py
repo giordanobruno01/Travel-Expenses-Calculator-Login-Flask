1095fdcc75c8c6cb859d51893ce995babee1711b
Giordano <giordanobrunotj@hotmail.com>
2024-03-16 12:21:14 +0000

    delete working

--- project/main.py
+++ project/main.py
@@ -6,25 +6,28 @@ import random, string
 from .models import User, Items, Trip
 
 main = Blueprint('main', __name__)
-
-currentTripId = None
+currentTripId = None 
 
 @main.route('/') 
 def index():
-    # currentTripId = None
+    global currentTripId      
+    currentTripId = None
+    
     return render_template('index.html')
 
 @main.route('/trips') 
 @login_required 
 def trips():
-    # currentTripId = None
+    global currentTripId 
+    currentTripId = None
     trip = Trip.query.filter_by(email = current_user.email).all()
     return render_template('trips.html', trip =trip)
 
 @main.route("/trips", methods=['POST'])
 @login_required
 def trips_post(): 
-    # currentTripId = None
+    global currentTripId 
+    currentTripId = None
     if current_user.is_authenticated:
         # add trip details
         TripName = request.form.get('TripName') 
@@ -50,24 +53,23 @@ def trips_post():
 @main.route('/myexpenses')
 @login_required 
 def myexpenses():
-
+    global currentTripId
 
     if currentTripId == None:
         return redirect(url_for("main.trips_post"))
     
     currentTripIdCopy = currentTripId
-    currentTripId = None
     
     item = Items.query.filter_by(email = current_user.email, tripId = currentTripIdCopy).all()
     total =0 
-
+    tripDetails = Trip.query.filter_by(id = currentTripId)
     for i in item:
         try:
             total = total+i.price
         except:
             continue
 
-    return render_template('myexpenses.html',name=current_user.name, item = item, total = total, tripId = currentTripIdCopy, tripName = Trip.startdate)
+    return render_template('myexpenses.html',name=current_user.name, item = item, total = total, tripId = currentTripIdCopy, tripName = str(tripDetails[0].trip + " " + tripDetails[0].startdate + " to " + tripDetails[0].enddate))
 
 
 @main.route('/myexpenses/<tripId>', methods = ["POST", "get"])
@@ -81,7 +83,7 @@ def myexpenses_post(tripId):
         item = request.form.get('Item') 
         price = request.form.get('PriceItem')
         date = request.form.get('DateItem')
-        
+         
         if item != None:
             new_item = Items(email=current_user.email, item=item, price =price, date =date, tripId = tripId)
             db.session.add(new_item)
@@ -91,25 +93,49 @@ def myexpenses_post(tripId):
     return redirect(url_for("main.myexpenses")) 
 
 
-
-@main.route('/delete', methods = ["POST"])
-def delete():
-    
-    id = request.form["id"] 
-    item = Items.query.get_or_404(id)
-    
-    db.session.delete(item)
-    db.session.commit()
-    return id  
-
 @main.route('/profile')
 @login_required
 def profile():
-    pass
+    global currentTripId 
+    currentTripId = None      
     # item = Items.query.filter_by(email = current_user.email).all()
     # total =0
     # for i in item:
     #     total = total+i.price
     return render_template('profile.html')
 
+@main.route('/deleteItem', methods = ["POST"])
+def deleteItem():
+    
+    id = request.form["id"] 
+    item = Items.query.get_or_404(id)
+    
+    db.session.delete(item)
+    db.session.commit()
+    return id   
+
+@main.route('/deleteItemAll', methods = ["POST"])
+def deleteItemAll():
+    id = request.form["tripId"] 
+    item = Items.query.filter_by(tripId = id).all()
+    for i in item:
+        db.session.delete(i)
+        db.session.commit()
+    return id  
+
+@main.route('/deleteTrip', methods = ["POST"])
+def deleteTrip():
+    
+    id = request.form["id"] 
+    trip = Trip.query.get_or_404(id)
+    
+    db.session.delete(trip)
+    db.session.commit()
+
+    item = Items.query.filter_by(tripId = id).all()
+    for i in item:
+        db.session.delete(i)
+        db.session.commit()
+    return id  
+ 
       
