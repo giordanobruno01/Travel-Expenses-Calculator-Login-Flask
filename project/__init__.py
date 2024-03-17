2d75405502b8bfb60dd5df1d8e2d03348a853eae
Giordano <giordanobrunotj@hotmail.com>
2024-03-17 10:33:47 +0000

    main file only

--- project/__init__.py
+++ project/__init__.py
@@ -1,44 +1,44 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-# from project import db, create_app, models
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
+# # from project import db, create_app, models
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)  
+# def create_app():
+#     app = Flask(__name__)  
 
-    app.config['SECRET_KEY'] = 'giordano-secrets'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+#     app.config['SECRET_KEY'] = 'giordano-secrets'
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
    
 
-    db.init_app(app)
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-    from .models import User
+#     db.init_app(app)
+#     login_manager = LoginManager()
+#     login_manager.login_view = 'auth.login'
+#     login_manager.init_app(app)
+#     from .models import User
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
+#     @login_manager.user_loader
+#     def load_user(user_id):
+#         # since the user_id is just the primary key of our user table, use it in the query for the user
+#         return User.query.get(int(user_id))
 
-    # db.create_all(app=create_app())
-    with app.app_context():
+#     # db.create_all(app=create_app())
+#     with app.app_context():
         
-        db.create_all()
+#         db.create_all()
 
-    # blueprint for auth routes in our app
-    from .auth import auth 
-    app.register_blueprint(auth)
+#     # blueprint for auth routes in our app
+#     from .auth import auth 
+#     app.register_blueprint(auth)
   
-    # blueprint for non-auth parts of app
-    from .main import main  
-    app.register_blueprint(main)
+#     # blueprint for non-auth parts of app
+#     from .main import main  
+#     app.register_blueprint(main)
 
-    return app
-    # export FLASK_APP=project
-    # export FLASK_DEBUG=1
-    # python3 -m venv auth
-# source auth/bin/activate
+#     return app
+#     # export FLASK_APP=project
+#     # export FLASK_DEBUG=1
+#     # python3 -m venv auth
+# # source auth/bin/activate
     
--- project/main.py
+++ project/main.py
@@ -1,53 +1,93 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_required, current_user
-from werkzeug.security import gen_salt
-from . import db
+from flask import Blueprint, render_template, redirect, url_for, request, flash, Flask
+from flask_login import login_user, login_required, logout_user, current_user, LoginManager, UserMixin
+from werkzeug.security import gen_salt, generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 import random, string
-from .models import User, Items, Trip
 
-main = Blueprint('main', __name__)
-currentTripId = None 
+# main = Blueprint('main', __name__)
 
-@main.route('/') 
+main = Flask(__name__)
+main.config['SECRET_KEY'] = 'giordano-secrets'
+main.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy()
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100))
+    trip = db.Column(db.String(1000))
+    startdate = db.Column(db.String(100))
+    enddate = db.Column(db.String(100))
+
+class Items(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(1000))
+    item = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+    date = db.Column(db.String(100))
+    tripId = db.Column(db.String(1000))
+
+db.init_app(main)
+with main.app_context():
+
+        db.create_all()
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(main)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
+
+currentTripId = None
+
+@main.route('/')
 def index():
-    global currentTripId      
+    global currentTripId
     currentTripId = None
-    
+
     return render_template('index.html')
 
-def convertDateFormat(date):
-    dates = date.split("-")
-    
-    newDate = dates[2] +"/"+ dates[1] +"/"+dates[0][2:]
-    return newDate
-    
-@main.route('/trips') 
-@login_required 
+@main.route('/trips')
+@login_required
 def trips():
-    global currentTripId  
+    global currentTripId
     currentTripId = None
     trip = Trip.query.filter_by(email = current_user.email).all()
     return render_template('trips.html', trip =trip)
 
+def convertDateFormat(date):
+    dates = date.split("-")
+
+    newDate = dates[2] +"/"+ dates[1] +"/"+dates[0][2:]
+    return newDate
+
 @main.route("/trips", methods=['POST'])
 @login_required
-def trips_post(): 
-    global currentTripId 
+def trips_post():
+    global currentTripId
     currentTripId = None
     if current_user.is_authenticated:
         # add trip details
-        TripName = request.form.get('TripName') 
+        TripName = request.form.get('TripName')
         StartDate = request.form.get('StartDate')
         EndDate = request.form.get('EndDate')
         email  = current_user.email
-       
+
         new_trip = Trip(email=email, trip = TripName, startdate = convertDateFormat(StartDate), enddate = convertDateFormat(EndDate))
         db.session.add(new_trip)
         db.session.commit()
-        flash('Trip added') 
-        
-        return redirect(url_for("main.trips"))
-    
+        flash('Trip added')
+
+        return redirect(url_for("trips"))
+
     if current_user.is_authenticated:
         item = Items.query.filter_by(email = current_user.email).all()
         total =0
@@ -57,17 +97,17 @@ def trips_post():
 
 
 @main.route('/myexpenses')
-@login_required 
+@login_required
 def myexpenses():
     global currentTripId
 
     if currentTripId == None:
-        return redirect(url_for("main.trips_post"))
-    
+        return redirect(url_for("trips_post"))
+
     currentTripIdCopy = currentTripId
-    
+
     item = Items.query.filter_by(email = current_user.email, tripId = currentTripIdCopy).all()
-    total =0 
+    total =0
     tripDetails = Trip.query.filter_by(id = currentTripId)
     for i in item:
         try:
@@ -82,28 +122,28 @@ def myexpenses():
 @login_required
 def myexpenses_post(tripId):
 
-    global currentTripId 
+    global currentTripId
     currentTripId = tripId
 
-    if current_user.is_authenticated: 
-        item = request.form.get('Item') 
+    if current_user.is_authenticated:
+        item = request.form.get('Item')
         price = request.form.get('PriceItem')
         date = request.form.get('DateItem')
-         
+
         if item != None:
-            new_item = Items(email=current_user.email, item=item, price =price, date = convertDateFormat(date), tripId = tripId)
+            new_item = Items(email=current_user.email, item=item, price =price, date =convertDateFormat(date), tripId = tripId)
             db.session.add(new_item)
             db.session.commit()
             flash('Expense added')
 
-    return redirect(url_for("main.myexpenses")) 
+    return redirect(url_for("myexpenses"))
 
 
 @main.route('/profile')
 @login_required
 def profile():
-    global currentTripId 
-    currentTripId = None      
+    global currentTripId
+    currentTripId = None
     # item = Items.query.filter_by(email = current_user.email).all()
     # total =0
     # for i in item:
@@ -112,29 +152,29 @@ def profile():
 
 @main.route('/deleteItem', methods = ["POST"])
 def deleteItem():
-    
-    id = request.form["id"] 
+
+    id = request.form["id"]
     item = Items.query.get_or_404(id)
-    
+
     db.session.delete(item)
     db.session.commit()
-    return id   
+    return id
 
 @main.route('/deleteItemAll', methods = ["POST"])
 def deleteItemAll():
-    id = request.form["tripId"] 
+    id = request.form["tripId"]
     item = Items.query.filter_by(tripId = id).all()
     for i in item:
         db.session.delete(i)
         db.session.commit()
-    return id  
+    return id
 
 @main.route('/deleteTrip', methods = ["POST"])
 def deleteTrip():
-    
-    id = request.form["id"] 
+
+    id = request.form["id"]
     trip = Trip.query.get_or_404(id)
-    
+
     db.session.delete(trip)
     db.session.commit()
 
@@ -142,6 +182,62 @@ def deleteTrip():
     for i in item:
         db.session.delete(i)
         db.session.commit()
-    return id  
- 
-      
+    return id
+
+
+@main.route('/login')
+def login():
+    return render_template('login.html', current_user =current_user)
+...
+@main.route('/login', methods=['POST'])
+def login_post():
+    # login code goes here
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return render_template('login.html', current_user =current_user) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return render_template('index.html', current_user =current_user)
+
+
+@main.route('/signup')
+def signup():
+    return render_template('signup.html', current_user =current_user)
+
+@main.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    try:
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+    except:
+        return "problem with get post"
+    try:
+        user = User.query.filter_by(email=email).first()
+    except:
+        return "problem with db"
+    if user:
+        flash('Email address already exists')
+        render_template('signup.html')
+    new_user = User(email=email, name=name, password=generate_password_hash(password,method='pbkdf2:sha256'))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return render_template('login.html')
+
+@main.route('/logout')
+def logout():
+    logout_user()
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    main.run(debug=True) 
